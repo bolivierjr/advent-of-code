a3779a2770469b9abd2ca93753dc671ad74f94f7
Bruce Olivier <bolivierjr@gmail.com>
2018-12-03 07:41:21 -0600

    adding day03 part 1 unfinished solution and input.txt

--- 2018/day03/day03-pt1.py
+++ 2018/day03/day03-pt1.py
@@ -1,11 +1,35 @@
 import os
+from typing import Dict, List, Deque
+from collections import deque
 
-directory: str = os.path.dirname(os.path.abspath(__file__))
-filename: str = os.path.join(directory, 'input.txt')
+
+def get_data() -> List[Dict[str, str]]:
+    directory: str = os.path.dirname(os.path.abspath(__file__))
+    filename: str = os.path.join(directory, 'input.txt')
+    data: List[dict] = []
+
+    with open(filename, 'r') as fp:
+        for line in fp:
+            column0: List[str] = line.split(' ')[0]
+            column2: List[str] = line.split(' ')[2].split(',')
+            column3: List[str] = line.split(' ')[3].split('x')
+
+            specs: Dict[str, str] = {
+                'claim': column0,
+                'left': column2[0],
+                'top': column2[1].rstrip(':'),
+                'width': column3[0],
+                'height': column3[1].rstrip('\n')
+            }
+
+            data.append(specs)
+
+    return data
 
 
-with open(filename, 'r') as fp:
-    for id_str in fp:
+if __name__ == '__main__':
+    data = get_data()
+    fabric = deque()
+
+    for specs in data:
         pass
-
-print(f'')
