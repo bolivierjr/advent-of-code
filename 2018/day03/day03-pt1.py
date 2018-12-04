23b46a6b26482ca8115bd7eebb37d95ccea9dbf9
Bruce Olivier <bolivierjr@gmail.com>
2018-12-03 21:12:02 -0600

    solution for day03 part 1

--- 2018/day03/day03-pt1.py
+++ 2018/day03/day03-pt1.py
@@ -1,25 +1,23 @@
 import os
 from typing import Dict, List, Deque
-from collections import deque
+from collections import deque, Counter
 
 
-def get_data() -> List[Dict[str, str]]:
+def get_data() -> List[Dict[str, int]]:
     directory: str = os.path.dirname(os.path.abspath(__file__))
     filename: str = os.path.join(directory, 'input.txt')
     data: List[dict] = []
 
     with open(filename, 'r') as fp:
         for line in fp:
-            column0: List[str] = line.split(' ')[0]
             column2: List[str] = line.split(' ')[2].split(',')
             column3: List[str] = line.split(' ')[3].split('x')
 
-            specs: Dict[str, str] = {
-                'claim': column0,
-                'left': column2[0],
-                'top': column2[1].rstrip(':'),
-                'width': column3[0],
-                'height': column3[1].rstrip('\n')
+            specs: Dict[str, int] = {
+                'left': int(column2[0]),
+                'top': int(column2[1].rstrip(':')),
+                'width': int(column3[0]),
+                'height': int(column3[1].rstrip('\n'))
             }
 
             data.append(specs)
@@ -29,10 +27,17 @@ def get_data() -> List[Dict[str, str]]:
 
 if __name__ == '__main__':
     data = get_data()
-    fabrics = deque([[], []])
+    fabrics = deque()
 
     for specs in data:
-        for iter, x in enumerate(range(0, int(specs['left']))):
-            if iter > len(fabrics[0]) - 1:
-                fabrics[0].append(None)
-    print(fabrics)
+        for x in range(specs['width']):
+            pass
+
+            for y in range(specs['height']):
+                x_coord = specs['left'] + x
+                y_coord = -specs['top'] + -y
+                fabrics.append((x_coord, y_coord))
+
+    overlap = len([x for x in Counter(fabrics).values() if x > 1])
+
+    print(f'Overlap: {overlap}')
