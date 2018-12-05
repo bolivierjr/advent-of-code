482ca31f8d51c02db7864a17f37f067762ec1a0a
Bruce Olivier <bolivierjr@gmail.com>
2018-12-05 17:40:43 -0600

    refactoring and changing data structure

--- 2018/day04/solution.py
+++ 2018/day04/solution.py
@@ -6,41 +6,45 @@ directory: str = os.path.dirname(os.path.abspath(__file__))
 filename: str = os.path.join(directory, 'input.txt')
 
 with open(filename, 'r') as fp:
-    logs: List[str] = sorted(fp.read().splitlines())
+    data: List[str] = sorted(fp.read().splitlines())
 
 
-def find_most_sleepy(data: List[str]) -> Tuple[str, int]:
-    queue: list = []
-    guards: dict = {}
+def find_most_sleepy(logs: List[str]) -> Tuple[str, int]:
+    queue = []
+    guards = {}
     sleep = 0
 
     for log in logs:
         raw_datetime, description = log.split(']')
         _, time = raw_datetime.split()
         _, minute = time.split(':')
+        guard_queue = queue[0]
 
         if 'Guard' in description:
             _, guard, _, _ = description.split()
 
             if guard not in guards:
-                guards[guard] = 0
+                guards[guard] = []
+
             if queue:
                 queue.pop()
+
             queue.append(guard)
-            continue
 
-        guard_queue = queue[0]
-
-        if guards and 'falls' in description:
+        elif guards and 'falls' in description:
             sleep += int(minute)
+
         elif guards and 'wakes' in description:
-            sleep = int(minute) - sleep
+            awake = int(minute) - sleep
+            for x in range(sleep, awake + 1):
+                pass
+
             guards[guard_queue] += sleep
-            sleep = 0
+            sleep, awake = 0, 0
     print(guards)
     return max(guards.items())
 
 
 if __name__ == '__main__':
-    sleepy_guard: Tuple[str, int] = find_most_sleepy(logs)
-    print(sleepy_guard)
+    find_most_sleepy(data)
+    print(data)
