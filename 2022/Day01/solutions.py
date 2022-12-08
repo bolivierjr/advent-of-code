641d449004676714c79770d8eac842b82b774cae
Bruce <bruce.olivier@bracketlabs.com>
2022-12-07 20:06:32 -0600

    Renamed some variables and methods

--- 2022/Day01/solutions.py
+++ 2022/Day01/solutions.py
@@ -7,16 +7,16 @@ from typing import Dict, Tuple
 
 
 def find_solution_one(data: str) -> Tuple[int, int]:
-    food_per_elf = format_data_by_food_per_elf(data)
-    elf_with_most_calories: int = max(food_per_elf, key=food_per_elf.get)
-    total_calories = food_per_elf.get(elf_with_most_calories, 0)
+    total_calories_per_elf = format_data_by_total_calories_per_elf(data)
+    elf_with_most_calories: int = max(total_calories_per_elf, key=total_calories_per_elf.get)
+    total_calories = total_calories_per_elf.get(elf_with_most_calories, 0)
 
     return elf_with_most_calories, total_calories
 
 
 def find_solution_two(data: str) -> int:
-    food_per_elf = format_data_by_food_per_elf(data)
-    sorted_total_calories_list = sorted(food_per_elf.values(), reverse=True)
+    total_calories_per_elf = format_data_by_total_calories_per_elf(data)
+    sorted_total_calories_list = sorted(total_calories_per_elf.values(), reverse=True)
     # top 3 calories from the elves with the most food in desc order
     top_three_most_calories = sorted_total_calories_list[0:3]
     sum_of_top_three_calories = sum(top_three_most_calories)
@@ -24,14 +24,14 @@ def find_solution_two(data: str) -> int:
     return sum_of_top_three_calories
 
 
-def format_data_by_food_per_elf(data: str) -> Dict[int, int]:
+def format_data_by_total_calories_per_elf(data: str) -> Dict[int, int]:
     # This ugly dictionary comprehension will format it like { 1: 10000, 2: 24000 }
-    food_per_elf = {
+    total_calories_per_elf = {
         elf + 1: sum(map(lambda calories: int(calories), food.split("\n")))
         for (elf, food) in enumerate(data.split("\n\n"))
     }
 
-    return food_per_elf
+    return total_calories_per_elf
 
 
 if __name__ == "__main__":
