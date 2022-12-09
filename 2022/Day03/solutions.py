3ec0b885050a5dcf0fa4a6679e9b4caaa2d703e0
Bruce Olivier <bolivierjr@gmail.com>
2022-12-09 10:42:34 -0600

    breaking out of the loops faster if found the matching item type for faster execution time

--- 2022/Day03/solutions.py
+++ 2022/Day03/solutions.py
@@ -25,14 +25,20 @@ def process_data(data: str) -> List[Tuple[str, str]]:
     return [(ruck[:len(ruck) // 2], ruck[len(ruck) // 2:]) for ruck in data.split("\n")]
 
 
-def find_sum_of_priorities(duplicate_types: str) -> int:
+def find_sum_of_priorities(duplicate_types: str):
     sum_of_priorities = 0
 
     for item_type in duplicate_types:
+        found_type = False
+
         for lower_index, lower_letter in enumerate(LOWER_ALPHA):
             if item_type == lower_letter:
+                found_type = True
                 sum_of_priorities += lower_index + 1  # 1 through 26
                 break
+
+        if found_type:
+            continue
 
         for upper_index, upper_letter in enumerate(UPPER_ALPHA):
             if item_type == upper_letter:
@@ -47,13 +53,16 @@ def find_duplicate_item_types(rucksacks: List[Tuple[str, str]]) -> str:
 
     for compartment_one, compartment_two in rucksacks:
         for item_in_one in compartment_one:
+            found_type = False
+
             for item_in_two in compartment_two:
                 if item_in_one == item_in_two:
+                    found_type = True
                     duplicate_types += item_in_one
                     break
-            else:
-                continue
-            break
+
+            if found_type:
+                break
 
     return duplicate_types
 
